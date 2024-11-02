27ca9ee307ebff31ab0bf4bc61ba44607982882d
Munnapd <abulhasem16015@gmail.com>
2024-11-02 21:56:27 +0600

    work_done

--- library/settings.py
+++ library/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000','https://*.127.0.0.1']
 # CSRF_TRUSTED_ORIGINS = ['https://libarary-management-system.onrender.com','https://*.127.0.0.1']
 
 
